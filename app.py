5599916ec4786bd9a5092aacb6c616cd16a57820
Usman Khalid <140155937+usman-dev-k@users.noreply.github.com>
2025-07-22 00:35:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pytesseract
 from gtts import gTTS
 from ultralytics import YOLO
 import tempfile
-from streamlit_webrtc import WebRtcMode, ClientSettings
+from streamlit_webrtc import WebRtcMode
 import av
 
 st.set_page_config(layout="wide")
